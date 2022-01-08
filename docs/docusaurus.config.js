d0dee67c9ebdb282f2df711d75c33661cab0958d
Badisi <badisi84@gmail.com>
2022-01-08 11:04:22 +0100

    fix(docs): edit links not working

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
                 docs: {
                     routeBasePath: '/',
                     sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl: 'https://github.com/DSI-HUG/ngx-schematics-utilities/edit/main/docs/docs/'
+                    editUrl: 'https://github.com/DSI-HUG/ngx-schematics-utilities/edit/main/docs/'
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css')
