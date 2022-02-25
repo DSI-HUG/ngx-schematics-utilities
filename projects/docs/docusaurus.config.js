df9f68e46f130c32e56d24923f173e468ba9c696
Badisi <badisi84@gmail.com>
2022-02-25 18:24:29 +0100

    fix(docs): disable breadcrumbs

--- projects/docs/docusaurus.config.js
+++ projects/docs/docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
+                    breadcrumbs: false,
                     routeBasePath: '/',
                     sidebarPath: require.resolve('./sidebars.js'),
                     editUrl: 'https://github.com/DSI-HUG/ngx-schematics-utilities/edit/main/docs/'
