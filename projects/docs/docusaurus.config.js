d6615b875fa87b896a58b395a78fd788798b0a39
Badisi <badisi84@gmail.com>
2023-01-30 12:35:49 +0100

    feat(docs): add search feature

--- projects/docs/docusaurus.config.js
+++ projects/docs/docusaurus.config.js
@@ -7,8 +7,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/nightOwl');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'ngx-schematics-utilities',
-    tagline: 'Useful utilities for Angular Schematics',
     url: 'https://dsi-hug.github.io',
+    tagline: 'Useful utilities for Angular Schematics',
     baseUrl: '/ngx-schematics-utilities/',
     organizationName: 'DSI-HUG',
     projectName: 'ngx-schematics-utilities',
@@ -17,20 +17,34 @@ const config = {
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
-
+    i18n: {
+        defaultLocale: 'fr-CH',
+        locales: ['fr-CH']
+    },
+    plugins: [
+        [
+            require.resolve('@cmfcmf/docusaurus-search-local'),
+            {
+                language: 'fr'
+            }
+        ]
+    ],
     presets: [
         [
-            '@docusaurus/preset-classic',
+            'classic',
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
-                    breadcrumbs: false,
                     routeBasePath: '/',
+                    breadcrumbs: false,
                     sidebarPath: require.resolve('./sidebars.js'),
                     editUrl: 'https://github.com/DSI-HUG/ngx-schematics-utilities/edit/main/docs/'
                 },
                 theme: {
-                    customCss: require.resolve('./src/css/custom.css')
+                    customCss: [
+                        require.resolve('./src/css/custom.css'),
+                        require.resolve('./src/css/search.css')
+                    ]
                 }
             })
         ]
@@ -41,55 +55,43 @@ const config = {
         ({
             navbar: {
                 title: 'ngx-schematics-utilities',
+                hideOnScroll: false,
                 logo: {
                     alt: 'logo',
                     src: 'img/logo.svg'
                 },
-                items: [
-                    {
-                        href: 'https://github.com/DSI-HUG/ngx-schematics-utilities',
-                        label: ' ',
-                        position: 'right',
-                        className: 'header-github-link',
-                        'aria-label': 'GitHub repository'
-                    }
-                ]
+                items: [{
+                    href: 'https://github.com/DSI-HUG/ngx-schematics-utilities',
+                    label: ' ',
+                    position: 'right',
+                    className: 'header-github-link',
+                    'aria-label': 'GitHub repository'
+                }]
             },
             footer: {
                 style: 'dark',
-                links: [
-                    {
-                        title: 'Docs',
-                        items: [
-                            {
-                                label: 'Installation',
-                                to: '/'
-                            },
-                            {
-                                label: 'APIs',
-                                to: '/apis/core'
-                            }
-                        ]
-                    },
-                    {
-                        title: 'Community',
-                        items: [
-                            {
-                                label: 'Stack Overflow',
-                                href: 'https://stackoverflow.com/questions/tagged/ngx-schematics-utilities'
-                            }
-                        ]
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            {
-                                label: 'GitHub',
-                                href: 'https://github.com/DSI-HUG/ngx-schematics-utilities'
-                            }
-                        ]
-                    }
-                ],
+                links: [{
+                    title: 'Docs',
+                    items: [{
+                        label: 'Installation',
+                        to: '/'
+                    }, {
+                        label: 'APIs',
+                        to: '/apis/core'
+                    }]
+                }, {
+                    title: 'Community',
+                    items: [{
+                        label: 'Stack Overflow',
+                        href: 'https://stackoverflow.com/questions/tagged/ngx-schematics-utilities'
+                    }]
+                }, {
+                    title: 'More',
+                    items: [{
+                        label: 'GitHub',
+                        href: 'https://github.com/DSI-HUG/ngx-schematics-utilities'
+                    }]
+                }],
                 copyright: `Copyright © ${new Date().getFullYear()} HUG`
             },
             prism: {
